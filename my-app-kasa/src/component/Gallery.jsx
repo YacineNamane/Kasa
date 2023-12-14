a5c840b7c428fc8351b13eafffddac4e9bee8fd6
yacine namane <yacine.nam@outlook.fr>
2023-12-14 15:10:17 +0100

    j'ai retravailler la structure de mon composant gallery afin de respecter les bonnes pratique et avoir un code plus clair et compréhensible

--- my-app-kasa/src/component/Gallery.jsx
+++ my-app-kasa/src/component/Gallery.jsx
@@ -1,12 +1,11 @@
 // Gallery.jsx
 import React, { useState } from "react";
-import Collapse from "react-bootstrap/Collapse";
 import ArrowRight from "../assets/vector.png";
 import ArrowLeft from "../assets/vector.png";
 
 const Gallery = ({ pictures }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [isCollapseOpen, setIsCollapseOpen] = useState(false);
+
   // ici j'affiche l'image précédente et si c'est la première image j'affiche la derière image : 2/4 > 1/4 /OU/ 1/4 > 4/4
   const handlePrevClick = () => {
     setCurrentImageIndex((prevIndex) =>
@@ -19,10 +18,6 @@ const Gallery = ({ pictures }) => {
       prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
     );
   };
-  // fonction qui gérre les mise a jour du collapse
-  const handleImageClick = () => {
-    setIsCollapseOpen(!isCollapseOpen);
-  };
 
   return (
     <div className="img-gallery-container">
@@ -33,7 +28,6 @@ const Gallery = ({ pictures }) => {
             key={index}
             src={picture}
             alt={`appartements ${index + 1}`}
-            onClick={handleImageClick}
             style={{
               display: index === currentImageIndex ? "block" : "none",
             }}
@@ -41,25 +35,23 @@ const Gallery = ({ pictures }) => {
         ))}
       </div>
 
-      <Collapse in={isCollapseOpen}>
-        <div className="arrow-container">
-          <img
-            src={ArrowLeft}
-            alt="Flèche gauche"
-            className="arrow-left"
-            onClick={handlePrevClick}
-          />
-          <span>
-            {currentImageIndex + 1}/{pictures.length}
-          </span>
-          <img
-            src={ArrowRight}
-            alt="Flèche droite"
-            className="arrow-right"
-            onClick={handleNextClick}
-          />
-        </div>
-      </Collapse>
+      <div className="arrow-container">
+        <img
+          src={ArrowLeft}
+          alt="Flèche gauche"
+          className="arrow-left"
+          onClick={handlePrevClick}
+        />
+        <span>
+          {currentImageIndex + 1}/{pictures.length}
+        </span>
+        <img
+          src={ArrowRight}
+          alt="Flèche droite"
+          className="arrow-right"
+          onClick={handleNextClick}
+        />
+      </div>
     </div>
   );
 };
